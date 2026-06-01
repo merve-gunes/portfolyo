8cb99256170721270d55f4f31efcf92e1ab535a5
Merve Gunes <mervegns283@gmail.com>
2026-06-01 15:31:56 +0300

    hakkimda bolumu eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Hakkimda from './components/Hakkimda'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Hakkimda />
     </div>
   )
 }
---
+++ src/components/Hakkimda.jsx
@@ -0,0 +1,23 @@
+import './Hakkimda.css'
+
+function Hakkimda(){
+    return (
+        <section className="hakkimda" id="hakkimda">
+            <h2>Hakkımda</h2>
+            <div className="hakkimda-content">
+                <div className="hakkimda-text">
+
+                <p>
+                            Merhaba! Ben Merve Safa Güneş, yazılım mühendisliği öğrencisiyim.Web geliştirme alanında kendimi geliştiriyorum ve yeni teknolojiler öğrenmekten büyük keyif alıyorum.
+                </p>
+
+                <p>
+                    Şu an staj yapıyor. React ile projeler geliştiriyorum.Kodlamayı bir problem çözme aracı olarak görüyorum.
+                </p>
+            </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hakkimda
