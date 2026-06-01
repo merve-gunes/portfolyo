532f90c16041d40ccb57874d7ddfb1263d518176
Merve Gunes <mervegns283@gmail.com>
2026-06-01 13:46:33 +0300

    navbar tamamlandı

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+
+function navbar () {
+
+    return (
+        <nav>
+            <h2>Merve Safa Güneş</h2>
+            <ul>
+                <li><a href="#hakkimda">Hakkımda</a></li>
+                <li><a href="#projeler">Projeler</a></li>
+                <li><a href="#yetenekler">Yetenekler</a></li>
+                <li><a href="#egitim">Eğitim</a></li>
+                <li><a href="#iletisim">İletişim</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default navbar
